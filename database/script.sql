d74011d6e20529dc8f7b85f171438960859af815
AnaDuarte1 <83419660+AnaDuarte1@users.noreply.github.com>
2022-11-08 17:50:19 -0300

    Update script.sql

--- database/script.sql
+++ database/script.sql
@@ -13,3 +13,34 @@ CREATE TABLE Usuario(
     
     PRIMARY KEY(id_usuario)
 );
+
+CREATE TABLE Tipo_Tecidos (
+    id_tipo_tecidos INT AUTO_INCREMENT,
+    nome_tecidos VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id_tipo_tecidos)
+);
+
+CREATE TABLE Tecidos (
+    id_tecidos INT AUTO_INCREMENT,
+    id_tipo_tecidos INT NOT NULL,
+    desc_tecidos VARCHAR(255) NOT NULL,
+    sustentavel VARCHAR(300) NOT NULL,
+
+    PRIMARY KEY (id_serie),
+    FOREIGN KEY (id_tipo_tecidos)
+        REFERENCES Tipo_Tecidos(id_tipo_tecidos)
+);
+
+INSERT INTO Tipo_Tecidos(nome_tecidos)
+VALUES  ('Algodão'),
+        ('Lã'),
+        ('Malha'),
+        ('Viscose'),
+        ('Seda'),
+        ('Linho'),
+        ('Poliester');
+
+INSERT INTO Tecidos(id_tecidos, desc_tecidos , sustentavel, nome_tecidos)
+VALUES (1,'Os tecidos de algodão são de fibra natural macia e por isso são conhecidos pelo seu conforto térmico, durabilidade e capacidade de se adaptar para todos',0,1),
+       (2,'A lã é derivada da pelagem da ovelha, da vicunha, da alpaca, da alpama ou da lhama que, depois de tosquiado, é processado industrialmente para usos têxteis, limpeza e coloração.',1067,2016,'Aventura pelo hospital',0,2);
