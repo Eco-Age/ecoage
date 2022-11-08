6c68868e58b3caa0cb2e32ab3546305f81d71708
gabi-ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2022-11-08 01:04:13 -0300

    Add files via upload

---
+++ src/portal_de_noticias.php
@@ -0,0 +1,87 @@
+<?php
+ session_start();
+
+ if(!isset($_SESSION["id_usuario"])){
+   header("Location: ../public/index.php");
+ }
+  include("../include/navegacao.php");  
+?>
+<div id="portal_de_noticias">
+    <h1 id="txt_portal">Portal de Notícias:</h1>
+        <div class="row">
+            <div class="col-4"></div>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar uma notícia..." aria-label="Pesquisar">
+                </form>
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#filtros" aria-expanded="false" aria-controls="collapseExample" id="btn_filtros">
+                Filtros<span class="material-symbols-outlined" id="seta_filtro">arrow_drop_down</span>
+                </button> 
+            <div class="col-4"></div>
+        </div>                
+        <div class="row">
+        <div class="col-4"></div>
+            <div class="collapse col-4" id="filtros">
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="filtros_" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Em qualquer data
+                    </button>
+                        <div class="dropdown-menu" aria-labelledby="filtros_">
+                            <a class="dropdown-item" href="#">Na última hora</a>
+                            <a class="dropdown-item" href="#">Nas últimas 24 horas</a>
+                            <a class="dropdown-item" href="#">Na última semana</a>
+                            <a class="dropdown-item" href="#">No último mês</a>
+                            <a class="dropdown-item" href="#">No último ano</a>
+                        </div>
+                </div>
+            </div>
+        <div class="col-4"></div>
+        </div>
+                      
+      <div class="row">
+            <div class="col-2"></div>
+            <div id="carrosel" class="carousel slide col-8 " data-ride="carousel">
+                <ol class="carousel-indicators">
+                <li data-target="#carrosel" data-slide-to="0" class="active"></li>
+                <li data-target="#carrosel" data-slide-to="1"></li>
+                <li data-target="#carrosel" data-slide-to="2"></li>
+                </ol>
+
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <a href="https://g1.globo.com/pop-arte/moda-e-beleza/noticia/2022/02/24/lixoes-texteis-as-imagens-que-mostram-como-a-industria-pode-ser-toxica-ao-meio-ambiente.ghtml"><img class="img_carrosel d-block w-100" src="../assets/noticia1.jpg" alt="Primeiro Slide"></a>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Lixões textêis</h5>
+                            <p>As imagens que mostram como a indústria pode ser tóxica ao meio ambiente...</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <a href="google.com"><img class="img_carrosel d-block w-100" src="../assets/logo.png" alt="Segundo Slide"></a>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Título da notícia</h5>
+                            <p>Descrição da notícia...</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <a href="google.com"><img class="img_carrosel d-block w-100" src="../assets/logo.png" alt="Terceiro Slide"></a>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Título da notícia</h5>
+                            <p>Descrição da notícia...</p>
+                        </div>
+                    </div>
+                </div>
+            
+                    <a class="carousel-control-prev" href="#carrosel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Anterior</span>
+                    </a>
+
+                    <a class="carousel-control-next" href="#carrosel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Próximo</span>
+                    </a>
+            </div>
+          <div class="col-2"></div>
+        </div>  
+    </div>
+</body>
+</html>
---
+++ src/tecidos.php
@@ -0,0 +1,42 @@
+<?php
+ session_start();
+
+ if(!isset($_SESSION["id_usuario"])){
+   header("Location: ../public/index.php");
+ }
+  include("../include/navegacao.php");  
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-1"></div>
+            <div class="card col-3">
+                    <img class="card-img-top" src="../assets/tecido.png" alt="Imagem de capa do card">
+                        <div class="card-body">
+                            <h5 class="card-title">Poliéster</h5>
+                            <p class="card-text">Você conquistou esse tecido!</p>
+                            <a class="btn btn-link" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Saber mais...</a>
+                                <div class="collapse" id="collapseExample">
+                                    <div class="card-body">
+                                        Poliéster é uma categoria de polímeros que, quimicamente falando, contém um grupo funcional éster na cadeia principal. Apesar de existirem muitos tipos de poliéster, o termo normalmente é usado para se referir ao politereftalato de etileno, ou PET. Sua composição pode ser natural e sintética, tornando alguns tipos biodegradáveis, enquanto grande parte dos poliésteres sintéticos não o são.
+                                    </div>
+                                </div>
+                        </div>
+            </div>
+            <div class="card col-3">
+                        <img class="card-img-top" src="../assets/tecido.png" alt="Imagem de capa do card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tecido</h5>
+                            <p class="card-text">Você conquistou esse tecido!</p>
+                            <a href="#" class="btn btn-link">Saber mais...</a>
+                        </div>
+            </div>
+            <div class="card col-3">
+                        <img class="card-img-top" src="../assets/tecido_bloqueado.png" alt="Imagem de capa do card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tecido</h5>
+                            <p class="card-text">Jogue para conquistar esse tecido.</p>
+                        </div>
+            </div>
+        <div class="col-1"></div>
+    </div>
+</div>
