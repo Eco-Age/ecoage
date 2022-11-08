5bd5c36c59ce3da9ac75aac5febc9724b7026721
gabi-ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2022-11-08 01:03:30 -0300

    Update pagina_inicial.php

---
+++ src/pagina_inicial.php
@@ -0,0 +1,18 @@
+<?php
+  session_start();
+
+  if(!isset($_SESSION["apelido_logado"]) && !isset($_SESSION["nome_logado"]) && !isset($_SESSION["id_usuario"])){
+    header("Location: ../public/index.php");
+  }
+  include("../include/navegacao.php");  
+?>
+<div class="container">
+  <?php 
+            if (isset($_SESSION["apelido_logado"])) { ?>
+              <p class="alert-success nav-link" id="logado">Logado como <?= $_SESSION["apelido_logado"] ?></p>
+  <?php } ?>
+  <h3 id="bemvindoo">Bem vindo(a) <?= $_SESSION["nome_logado"] ?>!</h3>
+</div>
+</body>
+</html>
+
