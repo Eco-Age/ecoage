c7ca1d5cfc184f23cd72f05694829fa09586f5ea
gabi-ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2022-11-09 17:17:19 -0300

    Update script.sql

--- database/script.sql
+++ database/script.sql
@@ -1,6 +1,8 @@
 USE ecoage;
 
 DROP TABLE IF EXISTS Usuario;
+DROP TABLE IF EXISTS Tecidos;
+DROP TABLE IF EXISTS Tipo_Tecidos;
 
 CREATE TABLE Usuario(
     id_usuario INT AUTO_INCREMENT,
@@ -14,6 +16,8 @@ CREATE TABLE Usuario(
     PRIMARY KEY(id_usuario)
 );
 
+
+
 CREATE TABLE Tipo_Tecidos (
     id_tipo_tecidos INT AUTO_INCREMENT,
     nome_tecidos VARCHAR(255) NOT NULL,
@@ -21,13 +25,13 @@ CREATE TABLE Tipo_Tecidos (
     PRIMARY KEY (id_tipo_tecidos)
 );
 
-CREATE TABLE Tecidos (
+CREATE TABLE Tecidos(
     id_tecidos INT AUTO_INCREMENT,
     id_tipo_tecidos INT NOT NULL,
     desc_tecidos VARCHAR(255) NOT NULL,
-    sustentavel VARCHAR(300) NOT NULL,
+    sustentavel BOOLEAN NOT NULL,
 
-    PRIMARY KEY (id_serie),
+    PRIMARY KEY (id_tecidos),
     FOREIGN KEY (id_tipo_tecidos)
         REFERENCES Tipo_Tecidos(id_tipo_tecidos)
 );
@@ -41,6 +45,6 @@ VALUES  ('Algodão'),
         ('Linho'),
         ('Poliester');
 
-INSERT INTO Tecidos(id_tecidos, desc_tecidos , sustentavel, nome_tecidos)
-VALUES (1,'Os tecidos de algodão são de fibra natural macia e por isso são conhecidos pelo seu conforto térmico, durabilidade e capacidade de se adaptar para todos',0,1),
-       (2,'A lã é derivada da pelagem da ovelha, da vicunha, da alpaca, da alpama ou da lhama que, depois de tosquiado, é processado industrialmente para usos têxteis, limpeza e coloração.',1067,2016,'Aventura pelo hospital',0,2);
+INSERT INTO Tecidos(id_tipo_tecidos, desc_tecidos , sustentavel)
+VALUES (1, 'Os tecidos de algodão são de fibra natural macia e por isso são conhecidos pelo seu conforto térmico, durabilidade e capacidade de se adaptar para todos',0),
+       (2, 'A lã é derivada da pelagem da ovelha, da vicunha, da alpaca, da alpama ou da lhama que, depois de tosquiado, é processado industrialmente para usos têxteis, limpeza e coloração.',0);
