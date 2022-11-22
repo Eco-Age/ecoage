6105a2d3e7bb86e1b9e61bbcd8c2e2c5b83df961
AnaDuarte1 <83419660+AnaDuarte1@users.noreply.github.com>
2022-11-22 17:33:13 -0300

    Update navegacao.php

--- include/navegacao.php
+++ include/navegacao.php
@@ -3,35 +3,39 @@
 include("cabecalho.php");
 
 ?>
-<body>    
-<header id="navegacao">
-<nav class="navbar navbar-expand-lg">
-  <div class="container">
-      <div class="row">
-        <ul class="navbar nav">
-          <li class="navbar-brand">
-          <a href="pagina_inicial.php" id=""> <img src="../assets/logo.png" width="50" height="50" alt="Eco Age"></a>
-          </li>
-          
-          <li class="navbar-text active">
-          <a class="nav-link " href="pagina_inicial.php" id="menu_home" autocomplete="off" checked><span class="material-icons" id="icone_home">home</span></a>
-          </li>
+ <header id="navegacao">
+        <nav class="navbar navbar-expand-lg sticky-top">
+         <div class="container">
+             <div class="row">
+               <ul class="navbar nav">
+                   <li class="navbar-brand col-1">
+                       <a href="../src/pagina_inicial.php" id=""> <img src="../assets/logo.png" width="50" height="50" alt="Eco Age"></a>
+                   </li>
+                   
+                   <li class="navbar-text active" >
+                       <a class="nav-link " href="../src/pagina_inicial.php" id="menu_home" autocomplete="off" checked><span class="material-icons" id="icone_home">home</span></a>
+                   </li>
+       
+                   <li class="navbar-text" >
+                       <a class="nav-link" href="../src/portal_de_noticias.php" id="menu_portal"><span >Portal de Notícias</span></a>
+                   </li>
+       
+                   <li class="navbar-text">
+                       <a class="nav-link" href="../src/tecidos.php" id="menu_tecidos">Tecidos</a>
+                   </li>
+                 
+                   <li class="navbar-text">
+                       <a class="nav-link" href="../src/jogo.php" id="game"><span class="material-symbols-outlined" id="icone_game">stadia_controller</span></a>
+                   </li>
+                   
+                   <li class="navbar-text" id="menu_ajuda">
+                       <a class="nav-link" href="../src/ajuda.php" id="ajuda"><span class="material-symbols-outlined">help</span></a>
+                   </li>
+       
+                   <li class="navbar-text" id="menu_sobre_nos">
+                       <a class="nav-link" href="../src/sobre_nos.php"  id="icone_sobrenos"><span class="material-symbols-outlined">groups</span></a>
+                  </li>
 
-          <li class="navbar-text" >
-            <a class="nav-link" href="portal_de_noticias.php" id="menu_portal"><span >Portal de Notícias</span></a>
-          </li>
-
-          <li class="navbar-text">
-            <a class="nav-link" href="tecidos.php" id="menu_tecidos">Tecidos</a>
-          </li>
-
-          <li class="navbar-text">
-            <a class="nav-link" href="jogo.php" id="game"><span class="material-symbols-outlined" id="icone_game">stadia_controller</span></a>
-          </li>
-
-          <li class="navbar-text" id="menu_ajuda">
-            <a class="nav-link" href="ajuda.php" id="ajuda"><span class="material-symbols-outlined" id="icone_ajuda">help</span></a>
-          </li>
 
             <li class="navbar-text dropdown" id="menu">
                 <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" id="menu_m"><span class="material-icons" id="icone_menu">menu</span></a>
@@ -41,7 +45,7 @@ include("cabecalho.php");
                               if (isset($_SESSION["id_usuario"])) { ?>
                               <form action="../src/edicao_usuario.php" method="get">
                                   <input type="hidden"  name="id_usuario" value="<?=$_SESSION["id_usuario"]?>"/>
-                                  <button type="submit" class="dropdown-item" id="button_perfil"><span class="material-icons" id="icone_perfil">account_circle</span>Meu perfil</button>
+                                  <button type="submit" class="dropdown-item"><span class="material-icons" id="icone_perfil">account_circle</span>Meu perfil</button>
                               </form>
                         <?php } ?>  
                     </div>
@@ -54,4 +58,4 @@ include("cabecalho.php");
          </div>
       </div>
     </nav>
-  </header>
+</header>  
