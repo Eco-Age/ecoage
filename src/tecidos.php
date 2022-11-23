592236333dd8ac225a653980da95f77c5e99170b
edu-boni <116756404+edu-boni@users.noreply.github.com>
2022-11-23 16:30:04 -0300

    Update tecidos.php

--- src/tecidos.php
+++ src/tecidos.php
@@ -4,42 +4,69 @@
  if(!isset($_SESSION["id_usuario"])){
    header("Location: ../public/index.php");
  }
-  include("../include/navegacao.php");  
+ if ($_SESSION["id_usuario"] == 1){
+    header("Location: ../src/tecidos_adm.php");
+}
+include("../include/navegacao.php");  
+
 ?>
-<div class="container">
-    <div class="row">
-        <div class="col-1"></div>
-            <div class="card col-3">
-                    <img class="card-img-top" src="../assets/tecido.png" alt="Imagem de capa do card">
-                        <div class="card-body">
-                            <h5 class="card-title">Poliéster</h5>
-                            <p class="card-text">Você conquistou esse tecido!</p>
-                            <a class="btn btn-link" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Saber mais...</a>
-                                <div class="collapse" id="collapseExample">
-                                    <div class="card-body">
-                                        Poliéster é uma categoria de polímeros que, quimicamente falando, contém um grupo funcional éster na cadeia principal. Apesar de existirem muitos tipos de poliéster, o termo normalmente é usado para se referir ao politereftalato de etileno, ou PET. Sua composição pode ser natural e sintética, tornando alguns tipos biodegradáveis, enquanto grande parte dos poliésteres sintéticos não o são.
-                                    </div>
-                                </div>
-                        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-8"></div>
+            <div class="col-4">
+                <button class="btn" onclick="confirmarvisaoadm()" id="visaoadm">
+                    Visualizar como administrador<span class="material-symbols-outlined" id="visao_usu">visibility</span>
+                </button>            </div>
+            <div class="col-0"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-4"></div>
+                    <h1 class="col-4" id="txt_tecidos">Tecidos Conquistados:</h1>
+            <div class="col-4"></div>
+        </div>
+
+
+        <div class="row" id="tecidos">
+            <div id="tecido1_usu">
+                    <img id="imgtecido1"class="card-img-top" src="../assets/tecido.png" alt="Poliéster">
+                <p>
+                    <h5>Poliester</h5>
+                        <p>Voce conquistou esse tecido!</p>
+                        <a class="btn btn-primary" id="saibamais4" data-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">
+                        Saiba mais..
+                        </a>
+                </p>
+                  
+                <div class="collapse" id="collapse1">
+                    <div class="card card-body"  id="card1">
+                        Poliéster é uma categoria de polímeros que, quimicamente falando, contém um grupo funcional éster na cadeia principal. Apesar de existirem muitos tipos de poliéster, o termo normalmente é usado para se referir ao politereftalato de etileno, ou PET. Sua composição pode ser natural e sintética, tornando alguns tipos biodegradáveis, enquanto grande parte dos poliésteres sintéticos não o são.
+                    </div>
+                </div>
             </div>
-            <div class="card col-3">
-                        <img class="card-img-top" src="../assets/tecido.png" alt="Imagem de capa do card">
-                        <div class="card-body">
-                            <h5 class="card-title">Tecido</h5>
-                            <p class="card-text">Você conquistou esse tecido!</p>
-                            <a href="#" class="btn btn-link">Saber mais...</a>
-                        </div>
+            <div  id="tecido2_usu">
+                <img id="imgtecido2" class="card-img-top" src="../assets/tecido_bloqueado.png" alt="Algodão">
+                    <p>
+                        <h5>Algodão</h5>
+                            <p>Jogue para desbloquear esse tecido</p>
+                            <a class="btn btn-secondary" data-toggle="" href="" role="" aria-expanded="" aria-controls="">
+                            Saiba mais..
+                            </a>                   
+                    </p>
             </div>
-            <div class="card col-3">
-                        <img class="card-img-top" src="../assets/tecido_bloqueado.png" alt="Imagem de capa do card">
-                        <div class="card-body">
-                            <h5 class="card-title">Tecido</h5>
-                            <p class="card-text">Jogue para conquistar esse tecido.</p>
-                        </div>
+            <div id="tecido3_usu">
+                <img id="imgtecido3" class="card-img-top" src="../assets/tecido_bloqueado.png" alt="Linho">
+                    <p>
+                        <h5>Linho</h5>
+                            <p>Jogue para desbloquear esse tecido</p>
+                            <a class="btn btn-secondary" data-toggle="" href="" role="" aria-expanded="" aria-controls="">
+                            Saiba mais..
+                            </a>
+                  </p>
             </div>
-        <div class="col-1"></div>
+        </div>    
     </div>
-</div>
-    <?php
+    <script src="../assets/script.js"></script>
+<?php
         include("../include/rodape.php");
-    ?>
+?>
