058b17c1c6b67551f32da8f38009afc0645e54a6
AnaDuarte1 <83419660+AnaDuarte1@users.noreply.github.com>
2022-11-29 17:36:44 -0300

    Update login.php

--- src/login.php
+++ src/login.php
@@ -4,9 +4,22 @@ require("../database/usuario.php");
 $apelido = $_POST["apelido_login"];
 $senha = $_POST["senha_login"];
 
-if(fazer_login($apelido, $senha)){
-    header("Location: ../src/pagina_inicial.php");
-}else{
-    header("Location: ../public/index.php");
+
+$dados_consulta = buscarUsuario($apelido, $senha);
+$usuario = $dados_consulta[0];
+
+if ($usuario == null) {
+  $saida = ["autenticado" => false, "msg" => $dados_consulta[1]];
+  header("Location: ../public/index.php");
+} else {
+  $saida = ["autenticado" => true];
+  $_SESSION["nome_logado"] = $usuario["nome_completo"];
+  $_SESSION["apelido_logado"] = $usuario["apelido"];
+  $_SESSION["id_usuario"] = $usuario["id_usuario"];
+  header("Location: ../src/pagina_inicial.php");
+
 }
+
+$json = json_encode($saida);
+echo $json;
 ?>
