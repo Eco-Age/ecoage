9bc1ba6ad5ff63334f2dee38b2740631eaabdb26
edu-boni <116756404+edu-boni@users.noreply.github.com>
2023-03-03 22:29:51 -0300

    Update recebe_duvidas.php

--- TCC/src/recebe_duvidas.php
+++ TCC/src/recebe_duvidas.php
@@ -1,19 +1,51 @@
 <?php
-    if (isset($_POST['email_ajuda']) && !empty($_POST['email_ajuda'])){
-        $email_ajuda = addslashes($_POST["email_ajuda"]);
-        $assunto_ajuda = addslashes($_POST["assunto_ajuda"]);
-        $duvida = addslashes($_POST["duvida"]);
+  include("../include/cabecalho.php");
+  require ("../database/usuario.php");
+  require_once ('PHPMailer-6.7.1/src/PHPMailer.php');
+  require_once ('PHPMailer-6.7.1/src/SMTP.php');
+  require_once ('PHPMailer-6.7.1/src/Exception.php');
 
-        $to = "eduboni10@gmail.com";
-        $subject = "Contato - EcoAge";
-        $body = "Email: ".$email_ajuda. "\r\n".
-                "Assunto: ".$assunto_ajuda. "\r\n".
-                "Dúvida: ".$duvida;
-        $header = "From: eduuboni@gmail.com"."\r\n"."Reply-To:".$email_ajuda."\e\n"."X=Mailer:PHP/".phpversion();
-        if(mail($to, $subject, $body, $header,'-feduboni10@gmail.com')){
-            echo ("Sua dúvida foi enviada e será respondida em breve!");
-        }else{
-            echo "Não pode ser enviado";
-        }
-    } 
+      $email_ajuda = $_POST['email_ajuda'];
+      $assunto_ajuda = $_POST["assunto_ajuda"];
+      $duvida = $_POST["duvida"];
+      $nome  = $_POST["nome"];
+      $mail = new PHPMailer\PHPMailer\PHPMailer();
+      
+      $mail->isSMTP();
+      $mail->SMTPDebug = 0;      
+      $mail->SMTPAuth = true;
+      $mail->SMTPSecure = 'tls';
+      $mail->Host = 'smtp.gmail.com';
+      $mail->Username = 'live.ecoage@gmail.com'; 
+      $mail->Password = 'rdapzocicdqxyqba'; 
+      $mail->Port = 587;
+      $mail->CharSet = 'UTF-8';
+
+      $mail->isHTML(true);
+      $mail->ContentType = 'text/html';
+      
+      $mail->setFrom($email_ajuda, $nome);
+      $mail->addAddress('live.ecoage@gmail.com');
+
+      
+      $mail->Subject = '[Dúvida - EcoAge] - '. $assunto_ajuda;
+      $mail->Body    = '<h2 style="text-align: center;">'. $assunto_ajuda .' </h2>
+                        <h3>Email do usuário: </h3>
+                            <p>'. $email_ajuda .'</p>
+                        <h3>Nome do usuário: </h3>
+                            <p>'. $nome .'</p>
+                        <h3>Dúvida do usuário: </h3>
+                            <p>' . $duvida . '</p>';
+                        
+
+      
+      if ($mail->send()) {
+        $_SESSION["msg"] = "Sua dúvida foi enviada e será respondida em breve!";
+        $_SESSION["tipo_msg"] = "alert-success"; 
+        header("Location: ajuda.php");
+      } else {
+        $_SESSION["msg"] = 'Erro ao enviar o e-mail: ' . $mail->ErrorInfo;
+        $_SESSION["tipo_msg"] = "alert-danger"; 
+        header("Location: ajuda.php");
+      }
 ?>
