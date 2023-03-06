90e1f8ee4c602249a363e14a04a756a56fb32d6d
edu-boni <116756404+edu-boni@users.noreply.github.com>
2023-03-06 15:38:13 -0300

    Update recebe_duvidas.php

--- TCC/src/recebe_duvidas.php
+++ TCC/src/recebe_duvidas.php
@@ -23,7 +23,7 @@
       $mail->SMTPOptions = array(
         'ssl' => array(
             'verify_peer' => false,
-            'verify_peer_name' => true,
+            'verify_peer_name' => false,
             'allow_self_signed' => true
         ));
 
