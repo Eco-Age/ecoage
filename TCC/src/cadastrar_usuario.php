0b33f50e77c19d91d900c7d71facb2fff721e0d8
AnaDuarte1 <83419660+AnaDuarte1@users.noreply.github.com>
2023-03-09 22:21:11 -0300

    Update cadastrar_usuario.php

--- TCC/src/cadastrar_usuario.php
+++ TCC/src/cadastrar_usuario.php
@@ -7,12 +7,13 @@ $tel = $_POST["tel"];
 $apelido = $_POST["apelido"];
 $email = $_POST["email_cadastro"];
 $senha = $_POST["senha_cadastro"];
+$id_avatar = $_POST["id_avatar"];
 
 if (verificarUsuarioCadastrado($apelido, $email) == true){
         $_SESSION["msg"] = "Email ou apelido já cadastrado no sistema!";
         $_SESSION["tipo_msg"] = "alert-danger";
 }else {
-    inserirUsuario($nome_completo, $data_nasc, $tel, $apelido, $email, $senha);
+    inserirUsuario($nome_completo, $data_nasc, $tel, $apelido, $email, $senha,$id_avatar);
 }
 
 header("Location: ../public/index.php");
