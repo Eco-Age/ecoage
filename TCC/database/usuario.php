9c3aff1eb3affad0651f3429428f319c236d4071
AnaDuarte1 <83419660+AnaDuarte1@users.noreply.github.com>
2023-03-09 22:15:49 -0300

    Update usuario.php

--- TCC/database/usuario.php
+++ TCC/database/usuario.php
@@ -13,26 +13,30 @@ if (!isset($_SESSION)) {
     $stmt->execute();
     $resultado = $stmt->get_result();
     $cadastrado = mysqli_num_rows($resultado);
-    $stmt->close();
     if (0 < $cadastrado) {
        return true;
     }else {
       return false;
     }
+  
+    $stmt->close();
+    $conexao->close();
+  
   }
   
-  function inserirUsuario($nome_completo, $data_nasc, $tel, $apelido, $email, $senha){
+  function inserirUsuario($nome_completo, $data_nasc, $tel, $apelido, $email, $senha, $id_avatar){
 
     $conexao = obterConexao();
     $senha_md5 = md5($senha);
   
-    $sql = "INSERT INTO Usuario (nome_completo, data_nasc, tel, apelido, email, senha) 
-            VALUES (?, ?, ?, ?, ?, ?)"; 
+    $sql = "INSERT INTO Usuario (nome_completo, data_nasc, tel, apelido, email, senha, id_avatar) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)"; 
 
     $conexao = obterConexao();
     $stmt = $conexao->prepare($sql);
-    $stmt->bind_param("ssssss", $nome_completo, $data_nasc, $tel, $apelido, $email, $senha_md5);   
+    $stmt->bind_param("ssssssi", $nome_completo, $data_nasc, $tel, $apelido, $email, $senha_md5, $id_avatar);   
     $stmt->execute();
+
 
     if ($stmt->affected_rows > 0) {
         $_SESSION["msg"] = "Cadastro efetuado com sucesso!";
@@ -43,6 +47,7 @@ if (!isset($_SESSION)) {
         $_SESSION["tipo_msg"] = "alert-danger";
   
       }
+
 
     $stmt->close();
     $conexao->close(); 
@@ -104,17 +109,19 @@ function buscarUsuarioLogado($id_usuario){
       return $usuario;  
 }
    
-function editarUsuario($nome_completo, $data_nasc, $tel, $apelido, $email, $senha, $id_usuario){
+function editarUsuario($nome_completo, $data_nasc, $tel, $apelido, $email, $senha, $id_usuario, $id_avatar){
+$conexao = obterConexao();
+$senha_md5 = md5($senha); 
   
 $sql = "UPDATE Usuario 
-        SET nome_completo = ?, data_nasc = ?, tel = ?, apelido = ?, email = ?, senha = ?
+        SET nome_completo = ?, data_nasc = ?, tel = ?, apelido = ?, email = ?, senha = ?, id_avatar = ?,
         WHERE id_usuario = ?";
 
-  $conexao = obterConexao();
 
   $stmt = $conexao->prepare($sql);
-  $stmt->bind_param("ssssssi", $nome_completo, $data_nasc, $tel, $apelido, $email, $senha, $id_usuario);
+  $stmt->bind_param("ssssssii", $nome_completo, $data_nasc, $tel, $apelido, $email, $senha_md5, $id_usuario, $id_avatar);
   $stmt->execute();
+
 
   if ($stmt->affected_rows > 0) {
     $_SESSION["msg"] = "Seus dados foram alterados!";
@@ -127,7 +134,8 @@ $sql = "UPDATE Usuario
   $stmt->close();
   $conexao->close();
 }
-    
+
+
 
 //------------------------------------------------------------------------------------------------
   
@@ -168,103 +176,5 @@ function removerUsuario($id_usuario) {
   $conexao->close();
 }
 
-// - DENTRO DESTA SEÇÃO ESTÃO CONTIDOS TODAS AS FUNÇÕES REFERENTES A RECUPERAR A SENHA
 
-date_default_timezone_set('America/Sao_Paulo');
-
-function token($email_recuperar, $token){
-  $conexao = obterConexao();
-  $data_expiracao = date('Y-m-d H:i:s', strtotime('+5 minutes'));
-  $sql = "INSERT INTO tokens (email, token, data_expiracao) VALUES (?, ?, ?)";
-  $stmt = $conexao->prepare($sql);
-  $stmt->bind_param('sss', $email_recuperar, $token, $data_expiracao);
-  $stmt->execute();
-  $stmt->close();
-  $conexao->close();
-}
-
-function verificatoken($email, $token_digitado){
-
-  $conexao = obterConexao();
-  $sql = "SELECT token, data_expiracao FROM tokens WHERE email = ?";
-  $stmt = $conexao->prepare($sql);
-  $stmt->bind_param("s", $email);
-  $stmt->execute();
-  $stmt->bind_result($token_armazenado, $data_expiracao);
-  $stmt->fetch();
-  
-  $diferenca_minutos = (time() - strtotime($data_expiracao)) / 60;
-  
-  if ($token_digitado === $token_armazenado && $diferenca_minutos < 5) {
-  header('Location: form_nova_senha.php');
-  exit;
-  } else {
-    include ("../include/cabecalho.php");
-    echo '<div id="conteudo">
-            <h1 style="text-align: center; padding: 50px;" class="display-2">
-              Ocorreu um erro
-            </h1>
-            <h3 style="text-align: center;" class="display-4">
-              Clique no botão de dúvida para entender 
-              <button style="text-align: center;" type="submit" onclick="duvida_token()" <span class="material-symbols-outlined">help</span></button><br>
-              <a href="../src/token.php" style="text-align: center; background-color: #623f80;" class="btn btn-outline-light">Voltar</a>
-            </h3>
-          </div>';
-    echo "<script src='../assets/script.js'></script>";
-    include ("../include/rodape.php");
-  }
-  
-  $stmt->close();
-  $conexao->close();
-  }
-
-  function excluirTokenExpirado() {
-    $conexao = obterConexao();
-    $sql = "DELETE FROM tokens WHERE data_expiracao <= NOW()";
-    $stmt = $conexao->prepare($sql);
-    $stmt->execute();
-    $stmt->close();
-    $conexao->close();
-  }
-  
-  function recuperaSenha($email, $nova_senha){
-      $conexao = obterConexao();
-      $sql = "UPDATE Usuario SET senha = ? WHERE email = ?";
-      $stmt = $conexao->prepare($sql);
-      $nova_senha_md5 = md5($nova_senha);
-      $stmt->bind_param("ss", $nova_senha_md5, $email);
-      $stmt->execute();
-      if ($stmt->affected_rows > 0) {
-        $_SESSION["msg"] = "Sua senha foi atualizada. Realize o Login!";
-        $_SESSION["tipo_msg"] = "alert-success";
-      } else {
-        $_SESSION["msg"] = "Seus dados não foram alterados! Erro: " . mysqli_error($conexao);
-        $_SESSION["tipo_msg"] = "alert-danger";
-      }  
-    
-      $stmt->close();
-      $conexao->close();
-  } 
-  
-  function verificaEmail($email_recuperar){
-    $conexao = obterConexao();
-    $sql = "SELECT * FROM Usuario WHERE email = ?";
-    $stmt = $conexao->prepare($sql);
-    $stmt->bind_param("s",$email_recuperar);
-    $stmt->execute();
-    $resultado = $stmt->get_result();
-    $qtd_email = mysqli_num_rows($resultado);
-
-    if ($qtd_email < 1){
-      $_SESSION["msg"] = "Esta conta não está cadastrada. Favor utilizar um e-mail cadastrado.";
-      $_SESSION["tipo_msg"] = "alert-danger";
-      header("Location: ../public/index.php");
-      exit();
-    }
-
-    return $qtd_email;
-
-  }
-
-  // - FIM DA SEÇÃO RECUPERAR SENHA 
 ?>
