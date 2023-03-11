7e412e2dfe9512aab860d2ed561e50a7a7bed629
edu-boni <116756404+edu-boni@users.noreply.github.com>
2023-03-11 01:50:48 -0300

    Update token.php

--- TCC/src/token.php
+++ TCC/src/token.php
@@ -16,6 +16,7 @@
                             <label for="token">Token: </label>
                             <input type="text" class="form-control" id="token" style="text-align: left;" placeholder="Token recebido no e-mail" name="token" required>
                             <small id="token_info" class="form-text text-muted">Não compartilhe este token com ninguém antes que ele expire.</small>
+                            <small id="token_info" class="form-text text-muted">Se não receber o código, lembre-se de checar a caixa de SPAM!</small>
                         </div>
                         <input type="submit" class="main-btn btn btn-primary" value="Verificar Token">
                         <a href="../public/index.php" class="btn btn-secondary">Voltar</a>
