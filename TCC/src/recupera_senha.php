1f4c1bb5b5b2c17e5bdc9b88162c23ab15d9569a
edu-boni <116756404+edu-boni@users.noreply.github.com>
2023-03-11 01:44:07 -0300

    Update recupera_senha.php

--- TCC/src/recupera_senha.php
+++ TCC/src/recupera_senha.php
@@ -1,9 +1,17 @@
 <?php 
 require("../database/usuario.php");
 
-$email = $_SESSION["email_recuperar"];
-$nova_senha = $_POST["senha1"];
 
-recuperaSenha($email, $nova_senha);
-header("Location: ../public/index.php");
+if ($_POST["funcao"] == "alterarsenha") {
+    $id_usuario = $_SESSION["id_usuario"];
+    $nova_senha = $_POST["senha1"];
+    alteraSenha($id_usuario, $nova_senha);
+    header("Location: ../src/pagina_inicial.php");
+}else if ($_POST["funcao"] == "recuperasenha"){
+    $email = $_SESSION["email_recuperar"];
+    $nova_senha = $_POST["senha1"];
+    recuperaSenha($email, $nova_senha);
+    header("Location: ../public/index.php");
+}
+
 ?>
