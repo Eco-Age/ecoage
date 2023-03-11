12a3b18abc99630335c4478899e240a307228440
edu-boni <116756404+edu-boni@users.noreply.github.com>
2023-03-11 01:35:02 -0300

    Update editar_usuario.php

--- TCC/src/editar_usuario.php
+++ TCC/src/editar_usuario.php
@@ -1,16 +1,22 @@
 <?php 
 require("../database/usuario.php");
 
+if(isset($_SESSION["id_usuario"])){
+    $id_usuario = $_SESSION["id_usuario"];
+}
+$senhaDigitada = $_POST["confirmar_senha"];
+
 $id_usuario = $_POST["id_usuario"];
 $nome_completo = $_POST["nome_completo"];
 $data_nasc = $_POST["data_nasc"];
 $tel = $_POST["tel"];
 $apelido = $_POST["apelido"];
 $email = $_POST["email_cadastro"];
-$senha = $_POST["senha_cadastro"];
 $id_avatar = $_POST["id_avatar"];
 
-editarUsuario($nome_completo, $data_nasc, $tel, $apelido, $email, $senha, $id_usuario, $id_avatar);
+
+
+editarUsuario($senhaDigitada, $nome_completo, $data_nasc, $tel, $apelido, $email, $id_usuario, $id_avatar);
 header("Location: edicao_usuario.php");
 
 ?>
