e856d19426a1e41ca5b8861b40938a2d8f4b5b82
edu <eduboni10@gmail.com>
2023-06-08 14:57:16 -0300

    força de vontade

--- TCC/assets/jogo.js
+++ TCC/assets/jogo.js
@@ -5,129 +5,129 @@ const ctx = canvas.getContext("2d");
 let frames = 0;
 const gravidade = 0.25;
 const personagem = {
-    x: 50,
-    y: canvas.height / 2,
-    width: 90,
-    height: 90,
-    velocidade: 0,
-    pulo: 4.6,
-    atualizar() {
-        this.velocidade += gravidade;
-        this.y += this.velocidade;
-    },
-    desenhar() {
-        const imagemPersonagem = new Image();
-        imagemPersonagem.src = '../assets/personagem.png';
-        ctx.drawImage(imagemPersonagem, this.x, this.y, this.width, this.height);
-    },
+  x: 50,
+  y: canvas.height / 2,
+  width: 90,
+  height: 90,
+  velocidade: 0,
+  pulo: 4.6,
+  atualizar() {
+    this.velocidade += gravidade;
+    this.y += this.velocidade;
+  },
+  desenhar() {
+    const imagemPersonagem = new Image();
+    imagemPersonagem.src = '../assets/personagem.png';
+    ctx.drawImage(imagemPersonagem, this.x, this.y, this.width, this.height);
+  },
 };
 
 // Função de atualização do jogo
 function atualizarJogo() {
-    frames++;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    personagem.atualizar();
-    personagem.desenhar();
-    requestAnimationFrame(atualizarJogo);
+  frames++;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  personagem.atualizar();
+  personagem.desenhar();
+  requestAnimationFrame(atualizarJogo);
 }
 
 
 const tubos = {
-    width: 80,
-    espaco: 220,
-    maxHeight: canvas.height / 2,
-    minHeight: 100,
-    velocidade: 2,
-    espacamentoEntreTubos: 300,
-    list: [],
-    atualizar() {
-        if (frames % 100 === 0) {
-            const height = Math.floor(Math.random() * (this.maxHeight - this.minHeight) + this.minHeight);
-            const tuboAnterior = this.list[this.list.length - 1];
-            const novaDistancia = tuboAnterior ? tuboAnterior.x + this.espacamentoEntreTubos : canvas.width;
-            this.list.push({
-                x: novaDistancia,
-                y: 0,
-                height,
-            });
-            this.list.push({
-                x: novaDistancia,
-                y: height + this.espaco,
-                height: canvas.height - height - this.espaco,
-            });
-            
-            const existeCarretel = Math.random() < 0.5;
-            if (existeCarretel) {
-              const carretelY = height + this.espaco / 2 - carreteis.height / 2;
-              let carretelSobreposto = false;
-              for (const tubo of this.list) {
-                  if (
-                      tubo.x <= canvas.width &&
-                      tubo.x + this.width >= canvas.width &&
-                      (tubo.y > carretelY || tubo.y + tubo.height < carretelY + carreteis.height)
-                  ) {
-                      carretelSobreposto = true;
-                      break;
-                  }
-              }
-              if (!carretelSobreposto) {
-                  carreteis.list.push({
-                      x: canvas.width,
-                      y: carretelY,
-                      width: carreteis.width,
-                      height: carreteis.height,
-                  });
-              }
-          }
-          
-        }
+  width: 80,
+  espaco: 220,
+  maxHeight: canvas.height / 2,
+  minHeight: 100,
+  velocidade: 2,
+  espacamentoEntreTubos: 300,
+  list: [],
+  atualizar() {
+    if (frames % 100 === 0) {
+      const height = Math.floor(Math.random() * (this.maxHeight - this.minHeight) + this.minHeight);
+      const tuboAnterior = this.list[this.list.length - 1];
+      const novaDistancia = tuboAnterior ? tuboAnterior.x + this.espacamentoEntreTubos : canvas.width;
+      this.list.push({
+        x: novaDistancia,
+        y: 0,
+        height,
+      });
+      this.list.push({
+        x: novaDistancia,
+        y: height + this.espaco,
+        height: canvas.height - height - this.espaco,
+      });
 
+      const existeCarretel = Math.random() < 0.5;
+      if (existeCarretel) {
+        const carretelY = height + this.espaco / 2 - carreteis.height / 2;
+        let carretelSobreposto = false;
         for (const tubo of this.list) {
-            tubo.x -= this.velocidade;
-            if (tubo.x + this.width < 0) this.list.shift();
-        }
-    },
-    desenhar() {
-        const imagemTubo = new Image();
-        imagemTubo.src = '../assets/tubo.png';
-        for (const tubo of this.list) {
-          ctx.save(); // Salva o estado atual do contexto
-          if (tubo.y === 0) {
-            // Inverte a imagem verticalmente se o tubo estiver no topo
-            ctx.scale(1, -1);
-            ctx.drawImage(imagemTubo, tubo.x, -tubo.y - tubo.height, this.width, tubo.height);
-          } else {
-            ctx.drawImage(imagemTubo, tubo.x, tubo.y, this.width, tubo.height);
+          if (
+            tubo.x <= canvas.width &&
+            tubo.x + this.width >= canvas.width &&
+            (tubo.y > carretelY || tubo.y + tubo.height < carretelY + carreteis.height)
+          ) {
+            carretelSobreposto = true;
+            break;
           }
-          ctx.restore(); // Restaura o estado anterior do contexto
         }
-    },
+        if (!carretelSobreposto) {
+          carreteis.list.push({
+            x: canvas.width,
+            y: carretelY,
+            width: carreteis.width,
+            height: carreteis.height,
+          });
+        }
+      }
+
+    }
+
+    for (const tubo of this.list) {
+      tubo.x -= this.velocidade;
+      if (tubo.x + this.width < 0) this.list.shift();
+    }
+  },
+  desenhar() {
+    const imagemTubo = new Image();
+    imagemTubo.src = '../assets/tubo.png';
+    for (const tubo of this.list) {
+      ctx.save(); // Salva o estado atual do contexto
+      if (tubo.y === 0) {
+        // Inverte a imagem verticalmente se o tubo estiver no topo
+        ctx.scale(1, -1);
+        ctx.drawImage(imagemTubo, tubo.x, -tubo.y - tubo.height, this.width, tubo.height);
+      } else {
+        ctx.drawImage(imagemTubo, tubo.x, tubo.y, this.width, tubo.height);
+      }
+      ctx.restore(); // Restaura o estado anterior do contexto
+    }
+  },
 };
 
 const carreteis = {
   width: 40,
   height: 40,
   list: [],
-  
+
   atualizar() {
-      for (const carretel of this.list) {
-          carretel.x -= tubos.velocidade;
-          if (carretel.x + this.width < 0) this.list.shift();
-      }
+    for (const carretel of this.list) {
+      carretel.x -= tubos.velocidade;
+      if (carretel.x + this.width < 0) this.list.shift();
+    }
   },
   desenhar() {
-      const imagemCarretel = new Image();
-      imagemCarretel.src = '../assets/carretel.png';
-      
-      // Calcular o deslocamento horizontal necessário para centralizar os carretéis
-      const deslocamentoHorizontal = (tubos.width - this.width) / 2;
+    const imagemCarretel = new Image();
+    imagemCarretel.src = '../assets/carretel.png';
 
-      for (const carretel of this.list) {
-          // Definir a posição x do carretel com o deslocamento horizontal
-          const x = carretel.x + deslocamentoHorizontal;
-          
-          ctx.drawImage(imagemCarretel, x, carretel.y, this.width, this.height);
-      }
+    // Calcular o deslocamento horizontal necessário para centralizar os carretéis
+    const deslocamentoHorizontal = (tubos.width - this.width) / 2;
+
+    for (const carretel of this.list) {
+      // Definir a posição x do carretel com o deslocamento horizontal
+      const x = carretel.x + deslocamentoHorizontal;
+
+      ctx.drawImage(imagemCarretel, x, carretel.y, this.width, this.height);
+    }
   },
 };
 
@@ -159,49 +159,49 @@ function collision(personagem, tubo) {
 
 
 function detectCollisions() {
-    for (const tubo of tubos.list) {
-        if (collision(personagem, tubo)) {
-            return true;
-        }
+  for (const tubo of tubos.list) {
+    if (collision(personagem, tubo)) {
+      return true;
     }
-    for (const carretel of carreteis.list) {
-        if (collision(personagem, carretel)) { // se ocorrer uma "colisão" entre o personagem e o carretel,retorna true e adiciona a pontuação de carreteis coletados
-            carreteisColetados++;
-            const index = carreteis.list.indexOf(carretel);
-            carreteis.list.splice(index, 1); // remove o carretel com o indice correto apenas 1 vez
-        }
+  }
+  for (const carretel of carreteis.list) {
+    if (collision(personagem, carretel)) { // se ocorrer uma "colisão" entre o personagem e o carretel,retorna true e adiciona a pontuação de carreteis coletados
+      carreteisColetados++;
+      const index = carreteis.list.indexOf(carretel);
+      carreteis.list.splice(index, 1); // remove o carretel com o indice correto apenas 1 vez
     }
+  }
 
-    return personagem.y <= 0 || personagem.y + personagem.height >= canvas.height;
+  return personagem.y <= 0 || personagem.y + personagem.height >= canvas.height;
 }
 
 
 function desenharPontuacao() {
-    //quadrado branco atrás do relógio
-    ctx.fillStyle = "white";
-    ctx.fillRect(800, 0, -120, 65);
+  //quadrado branco atrás do relógio
+  ctx.fillStyle = "white";
+  ctx.fillRect(800, 0, -120, 65);
 
-    // Desenhar imagem de um carretel
-    const carretelImg = new Image();
-    carretelImg.src = "../assets/carretel.png";
-    ctx.drawImage(carretelImg, 10, 10, 20, 20);
+  // Desenhar imagem de um carretel
+  const carretelImg = new Image();
+  carretelImg.src = "../assets/carretel.png";
+  ctx.drawImage(carretelImg, 10, 10, 20, 20);
 
-    // Desenhar imagem de um relógio
-    const relogioImg = new Image();
-    relogioImg.src = "../assets/relogio.png";
-    ctx.drawImage(relogioImg, 750, 15, 40, 40);
+  // Desenhar imagem de um relógio
+  const relogioImg = new Image();
+  relogioImg.src = "../assets/relogio.png";
+  ctx.drawImage(relogioImg, 750, 15, 40, 40);
 
-    ctx.font = "20px Arial";
-    ctx.fillStyle = "black";
-    ctx.fillText(`${carreteisColetados}`, 40, 30);
-    ctx.fillText(`${formatarTempo(tempoDecorrido)}`, 700, 45);
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "black";
+  ctx.fillText(`${carreteisColetados}`, 40, 30);
+  ctx.fillText(`${formatarTempo(tempoDecorrido)}`, 700, 45);
 
-    function pausarTempo() {
-        if (!jogoFinalizado && tempoInicial) { // jogoFinalizado é falso, tempoInicial não é nulo, garante que o tempo só será pausado se o jogo estiver em andamento
-            tempoPausado = performance.now() - tempoInicial; //tempo percorrido até a pausa , performance.now() retorna o tempo atual,  ex: 10-0 = 10 segundos
-            jogoFinalizado = true; //jogo finalizado
-        }
+  function pausarTempo() {
+    if (!jogoFinalizado && tempoInicial) { // jogoFinalizado é falso, tempoInicial não é nulo, garante que o tempo só será pausado se o jogo estiver em andamento
+      tempoPausado = performance.now() - tempoInicial; //tempo percorrido até a pausa , performance.now() retorna o tempo atual,  ex: 10-0 = 10 segundos
+      jogoFinalizado = true; //jogo finalizado
     }
+  }
 }
 
 
@@ -216,9 +216,9 @@ function formatarTempo(tempo) {
 }
 
 function atualizarTempoDecorrido(timestamp) {
-  if (!tempoInicial){
+  if (!tempoInicial) {
     tempoInicial = timestamp - tempoPausado;
-  } 
+  }
 
   const tempoPassado = Math.floor((timestamp - tempoInicial) / 1000); // Calcula o tempo passado em segundos
   tempoDecorrido = tempoPassado;
@@ -384,119 +384,163 @@ $(document).ready(function () {
   }
 });
 
+function gameLoop() {
+  let jogoIniciado = false;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  frames = 0;
+  personagem.y = canvas.height / 2;
+  personagem.velocidade = 0;
+  tubos.list = [];
+  carreteis.list = [];
+  carreteisColetados = 0;
+  tempoDecorrido = 0;
+  tempoInicial = null;
 
+  const buttonWidth = 300;
+  const buttonHeight = 50;
+  const buttonX = canvas.width / 2 - buttonWidth / 2;
+  const buttonY = canvas.height / 2 - buttonHeight / 2; // Centralizado verticalmente
 
-    function gameLoop() {
-      const buttonWidth = 300;
-      const buttonHeight = 50;
-      const buttonX = canvas.width / 2 - buttonWidth / 2;
-      const buttonY = canvas.height / 2 - buttonHeight / 2; // Centralizado verticalmente
-    
-      ctx.fillStyle = "#8614e9";
-      ctx.fillRect(buttonX, buttonY, buttonWidth, buttonHeight);
-    
-      ctx.fillStyle = "black";
-      ctx.font = "30px Arial";
-      const buttonText = "Iniciar";
-      const buttonTextX = buttonX + buttonWidth / 2 - ctx.measureText(buttonText).width / 2;
-      const buttonTextY = buttonY + buttonHeight / 2 + 10;
-      ctx.fillText(buttonText, buttonTextX, buttonTextY);
-    
-      canvas.addEventListener('click', function(event) {
-        const rect = canvas.getBoundingClientRect();
-        const mouseX = event.clientX - rect.left;
-        const mouseY = event.clientY - rect.top;
-    
-        // Verifica se o clique ocorreu dentro dos limites do botão
-        if (mouseX >= buttonX && mouseX <= buttonX + buttonWidth &&
-            mouseY >= buttonY && mouseY <= buttonY + buttonHeight) {
-          contagemRegressiva();
-        }
-      });
-    }
+  const button = {
+    x: buttonX,
+    y: buttonY,
+    width: buttonWidth,
+    height: buttonHeight,
+    isHovered: false
+  };
 
+  function drawButton() {
+    ctx.fillStyle = button.isHovered ? "#a45ceb" : "#8614e9";
+    ctx.fillRect(button.x, button.y, button.width, button.height);
 
-    
-
-  function startGameLoop() {    
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    personagem.atualizar();
-    tubos.atualizar();
-    carreteis.atualizar();
-  
-  
-    personagem.desenhar();
-    tubos.desenhar();
-    carreteis.desenhar();
-    desenharPontuacao();
-  
-    if (keys[SPACE_BAR_KEY_CODE] || keys[UP_ARROW_KEY_CODE]) {
-      personagem.velocidade = -personagem.pulo;
-    }
-  
-    const colisaoComTubo = detectCollisions();
-    if (colisaoComTubo) {
-      gameOver();
-      return;
-    }
-  
-    for (const carretel of carreteis.list) {
-      if (collision(personagem, carretel)) {
-        carreteisColetados++;
-        const index = carreteis.list.indexOf(carretel);
-        carreteis.list.splice(index, 1);
-      }
-    }
-  
-    frames++;
-    requestAnimationFrame(startGameLoop);
+    ctx.fillStyle = "black";
+    ctx.font = "30px Arial";
+    const buttonText = "Iniciar";
+    const buttonTextX = button.x + button.width / 2 - ctx.measureText(buttonText).width / 2;
+    const buttonTextY = button.y + button.height / 2 + 10;
+    ctx.fillText(buttonText, buttonTextX, buttonTextY);
   }
+
+  // Função para verificar se o mouse está sobre o botão
+  function checkHover(event) {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    button.isHovered = (mouseX >= button.x && mouseX <= button.x + button.width &&
+      mouseY >= button.y && mouseY <= button.y + button.height);
+    canvas.style.cursor = button.isHovered ? "pointer" : "default";
+    drawButton();
+  }
+
+  // Evento de quando o mouse entra no botão
+  canvas.addEventListener('mousemove', checkHover);
+
+  // Evento de quando o mouse sai de cima do botão
+  canvas.addEventListener('mouseout', function (event) {
+    button.isHovered = false;
+    canvas.style.cursor = "default";
+  });
+
+  canvas.addEventListener('click', function (event) {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    if (mouseX >= button.x && mouseX <= button.x + button.width &&
+      mouseY >= button.y && mouseY <= button.y + button.height &&
+      !jogoIniciado) {
+      jogoIniciado = true;
+      canvas.removeEventListener('mousemove', checkHover);
+      canvas.removeEventListener('mouseout', arguments.callee);
+      canvas.removeEventListener('click', gameLoop);
+      canvas.style.cursor = "default";
+      contagemRegressiva();
+    }
+  });
+
+  drawButton();
+}
+
+function startGameLoop() {
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  personagem.atualizar();
+  tubos.atualizar();
+  carreteis.atualizar();
+
+
+  personagem.desenhar();
+  tubos.desenhar();
+  carreteis.desenhar();
+  desenharPontuacao();
+
+  if (keys[SPACE_BAR_KEY_CODE] || keys[UP_ARROW_KEY_CODE]) {
+    personagem.velocidade = -personagem.pulo;
+  }
+
+  const colisaoComTubo = detectCollisions();
+  if (colisaoComTubo) {
+    gameOver();
+    return;
+  }
+
+  for (const carretel of carreteis.list) {
+    if (collision(personagem, carretel)) {
+      carreteisColetados++;
+      const index = carreteis.list.indexOf(carretel);
+      carreteis.list.splice(index, 1);
+    }
+  }
+
+  frames++;
+  requestAnimationFrame(startGameLoop);
+}
 
 function gameOver() {
-    Swal.fire({ 
-      title: "Fim de jogo!",  
-      html: 
-        "<img id='imgpersonagem' src='../assets/personagemtriste.png'>" +
-        "<p>Carretéis coletados: " + carreteisColetados + "</p>" +
-        "<p>Tempo decorrido: " + formatarTempo(tempoDecorrido) + "</p>" +
-        "<br>",  
-      confirmButtonColor: '#8614e9',
-      showCancelButton: false,
-      confirmButtonText: 'Jogar Novamente',
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-      showCloseButton: true, // Adiciona o botão de fechar
-    })
-      .then((result) => {
-        if (result.value) {
-          restartGame();
-        }
-       else{
-          gameLoop();
-        }
-      });
-  }
-  
+  Swal.fire({
+    title: "Fim de jogo!",
+    html:
+      "<img id='imgpersonagem' src='../assets/personagemtriste.png'>" +
+      "<p>Carretéis coletados: " + carreteisColetados + "</p>" +
+      "<p>Tempo decorrido: " + formatarTempo(tempoDecorrido) + "</p>" +
+      "<br>",
+    confirmButtonColor: '#8614e9',
+    showCancelButton: false,
+    confirmButtonText: 'Jogar Novamente',
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    showCloseButton: true, // Adiciona o botão de fechar
+  })
+    .then((result) => {
+      if (result.value) {
+        restartGame();
+      }
+      else {
+        gameLoop();
+      }
+    });
+}
+
 function restartGame() {
-        
-    frames = 0;
-    personagem.y = canvas.height / 2;
-    personagem.velocidade = 0;
-    tubos.list = [];
-    carreteis.list = [];
-    carreteisColetados = 0;
-    tempoDecorrido = 0;
-    tempoInicial = null;
 
-    contagemRegressiva(); 
- }
+  frames = 0;
+  personagem.y = canvas.height / 2;
+  personagem.velocidade = 0;
+  tubos.list = [];
+  carreteis.list = [];
+  carreteisColetados = 0;
+  tempoDecorrido = 0;
+  tempoInicial = null;
 
-    function atualizartempoDecorrido() {
-    tempoDecorrido++;
-    setTimeout(atualizartempoDecorrido, 1000);
-} 
+  contagemRegressiva();
+}
+
+function atualizartempoDecorrido() {
+  tempoDecorrido++;
+  setTimeout(atualizartempoDecorrido, 1000);
+}
 
 gameLoop();
